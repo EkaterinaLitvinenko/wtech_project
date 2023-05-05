5e540a7d2585d53fd7532e5917b332be864eebf4
EkaterinaLitvinenko <71759726+EkaterinaLitvinenko@users.noreply.github.com>
2023-05-06 00:40:53 +0200

    redirection edited

--- app/Http/Middleware/AdminMiddleware.php
+++ app/Http/Middleware/AdminMiddleware.php
@@ -10,10 +10,10 @@ class AdminMiddleware
 {
     public function handle(Request $request, Closure $next)
     {
-        if (auth()->check() && auth()->user()->role === 'admin') {
-            return $next($request);
+        if (!auth()->check() || auth()->user()->role !== 'admin') {
+            return redirect('/admin/login');
         }
 
-        abort(403, 'Unauthorized action.');
+        return $next($request);
     }
 }
