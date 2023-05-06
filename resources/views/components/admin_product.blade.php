e7c7db2c5ae2328720c1686838e48f03ec040db9
RobJun <rob.junas@gmail.com>
2023-05-06 10:43:30 +0200

    drobne fixy pr eple autora

--- resources/views/components/admin_product.blade.php
+++ resources/views/components/admin_product.blade.php
@@ -5,7 +5,7 @@
     <h2 class="book-title">{{$book->book->title}}</h2>
     <p class="book-author">{{$book->authors}}</p>
     <p class="book-genre">{{$book->book->genre->name}}</p>
-    <p class="book-price">{{$book->book->price}}</p>
+    <p class="book-price">{{$book->book->price}}€</p>
     <div class="action-btn">
         <a href="/admin/edit/{{$book->book->id}}" class="edit-btn"><span class="fa-solid fa-pen-to-square"></span></a>
         <button
